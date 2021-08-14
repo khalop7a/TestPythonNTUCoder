c60a4681431468a1251f261b11bf9d2d290178ff
Minh Kha <khalop7a@gmail.com>
2021-08-14 19:16:42 +0700

    Vong Tay

--- VongTay.py
+++ VongTay.py
@@ -1,6 +1,6 @@
 n = int(input())
-kq = n % 7
+kq = (n-1) % 7 + 1
 if ((n - 1) // 7) % 2 == 0:
     print(str(7 - kq) + " " + str(kq))
 else:
-    print(str(kq) + " " + str(7-kq))
+    print("{} {}".format(kq, 7 - kq))
